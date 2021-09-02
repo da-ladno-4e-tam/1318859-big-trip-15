18b6239f9be62fce037f6d6f868440df50505f2a
da-ladno-4e-tam <mixa.awesome@gmail.com>
2021-09-02 12:51:41 +0300

    Меняй-удаляй ч.2 (коммит 7.2.6)

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -3,13 +3,135 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 import SmartView from './smart.js';
 import {countAllEvents} from '../utils/statistics.js';
 
-const renderMoneyChart = (moneyCtx, points) => {
+const renderMoneyChart = (moneyCtx, points) => new Chart(moneyCtx, {
+  plugins: [ChartDataLabels],
+  type: 'horizontalBar',
+  data: {
+    labels: ['TAXI', 'BUS', 'TRAIN', 'SHIP', 'TRANSPORT', 'DRIVE'],
+    datasets: [{
+      data: [400, 300, 200, 160, 150, 100],
+      backgroundColor: '#ffffff',
+      hoverBackgroundColor: '#ffffff',
+      anchor: 'start',
+    }],
+  },
+  options: {
+    plugins: {
+      datalabels: {
+        font: {
+          size: 13,
+        },
+        color: '#000000',
+        anchor: 'end',
+        align: 'start',
+        formatter: (val) => '€ ${val}',
+      },
+    },
+    title: {
+      display: true,
+      text: 'MONEY',
+      fontColor: '#000000',
+      fontSize: 23,
+      position: 'left',
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: '#000000',
+          padding: 5,
+          fontSize: 13,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false,
+        },
+        barThickness: 44,
+      }],
+      xAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false,
+        },
+        minBarLength: 50,
+      }],
+    },
+    legend: {
+      display: false,
+    },
+    tooltips: {
+      enabled: false,
+    },
+  },
+});
 
-};
-
-const renderTypeChart = (typeCtx, points) => {
-
-};
+const renderTypeChart = (typeCtx, points) => new Chart(typeCtx, {
+  plugins: [ChartDataLabels],
+  type: 'horizontalBar',
+  data: {
+    labels: ['TAXI', 'BUS', 'TRAIN', 'SHIP', 'TRANSPORT', 'DRIVE'],
+    datasets: [{
+      data: [4, 3, 2, 1, 1, 1],
+      backgroundColor: '#ffffff',
+      hoverBackgroundColor: '#ffffff',
+      anchor: 'start',
+    }],
+  },
+  options: {
+    plugins: {
+      datalabels: {
+        font: {
+          size: 13,
+        },
+        color: '#000000',
+        anchor: 'end',
+        align: 'start',
+        formatter: (val) => '${val}x',
+      },
+    },
+    title: {
+      display: true,
+      text: 'TYPE',
+      fontColor: '#000000',
+      fontSize: 23,
+      position: 'left',
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: '#000000',
+          padding: 5,
+          fontSize: 13,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false,
+        },
+        barThickness: 44,
+      }],
+      xAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false,
+        },
+        minBarLength: 50,
+      }],
+    },
+    legend: {
+      display: false,
+    },
+    tooltips: {
+      enabled: false,
+    },
+  },
+});
 
 const renderTimeChart = (timeCtx, points) => {
 
